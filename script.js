14f6ae12e642ee65f14cb5468c1983c3f64b63ce
shadab <shadabathar.cse@gmail.com>
2022-01-31 15:59:38 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function forclear() {
+    document.getElementById("output").innerHTML = "0";
+}
+
+function removeZero() {
+    var value = document.getElementById("output").innerHTML;
+    if (value == "0") {
+         value = " "
+         document.getElementById("output").innerHTML = value;
+    }
+}
+function solve() {
+    removeZero()
+    var equation = document.getElementById("output").innerHTML;
+    var solved = eval(equation);
+    document.getElementById('output').innerHTML = solved;
+}
+function fordisplay(value) {
+    removeZero()
+    document.getElementById("output").innerHTML += value;
+}
